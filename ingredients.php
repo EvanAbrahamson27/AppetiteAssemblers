3d49c7da40be0588037383dd42767f5959911819
EvanAbrahamson27 <evanabrahamson27@gmail.com>
2024-05-30 06:11:33 -0700

    Added Recipe functionality, as well as Ingredient Page
    Also had to change the config.php for my client, but left it for git. Will likely work on Search bar and further func next

---
+++ ingredients.php
@@ -0,0 +1,103 @@
+<?php require_once('config.php'); ?>
+<!-- TCSS 445 : Spring 2024 -->
+<!-- Assignment 4 Template -->
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Appetite Assemblers</title>
+        <!-- add a reference to the external stylesheet -->
+        <link rel="stylesheet" href="bootstrap.min.css">
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="index.php">Appetite Assemblers</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarColor01">
+                    <ul class="navbar-nav me-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">Recipes</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="restrictions.php">Diets + Lifestyles</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="authors.php">Authors</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="about.php">About</a>
+                        </li>
+                    </ul>
+                    <form class="d-flex">
+                        <input class="form-control me-sm-2" type="search" placeholder="Search">
+                        <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+        <div class="jumbotron">
+            <p class="lead">Now Displaying your Recipe<p>
+            <hr class="my-4">
+            <form method="GET" action="ingredients.php">
+                    <?php
+                    $connection = mysqli_connect(DBHOST, DBUSER, DBPASS, DBNAME);
+                    if ( mysqli_connect_errno() )
+                    {
+                        die( mysqli_connect_error() );
+                    }
+                    ?>
+                <?php
+                if ($_SERVER["REQUEST_METHOD"] == "GET")
+                {
+                    if (isset($_GET['id']) )
+                    {
+                ?>
+                <p>&nbsp;</p>
+                <table class="table table-hover">
+                    <thead>
+                        <tr class="table-success">
+                            <th scope="col">Ingredient Name</th>
+                            <th scope="col">Ingredient Price</th>
+                            <th scope="col">Ingredient Category</th>
+                        </tr>
+                    </thead>
+                    <?php
+                        if ( mysqli_connect_errno() )
+                        {
+                            die( mysqli_connect_error() );
+                        }
+                        $sql = " SELECT *
+                            FROM RECIPE_INGREDIENTS u
+                            LEFT JOIN INGREDIENTS i ON u.Ingredient_name = i.Ingredient_name
+                            WHERE u.Recipe_id = '{$_GET['id']}'
+                            ORDER BY i.Ingredient_cat ASC";
+                        if ($result = mysqli_query($connection, $sql))
+                        {
+                            while($row = mysqli_fetch_assoc($result))
+                            {
+                    ?>
+                    <tr>
+                        <td><?php echo $row['Ingredient_name'] ?></td>
+                        <td><?php echo '$' . $row['Ingredient_price'] ?></td>
+                        <td><?php echo $row['Ingredient_cat'] ?></td>
+                    </tr>
+                    <?php
+                            }
+                            // release the memory used by the result set
+                            mysqli_free_result($result);
+                        }
+                    } // end if (isset)
+                } // end if ($_SERVER)
+                    ?>
+                </table>
+            </form>
+        </div>
+    </body>
+</html>
